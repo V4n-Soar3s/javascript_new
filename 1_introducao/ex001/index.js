84ee625f22f26de62a6ccaac7714d4939aa97f37
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-10 23:51:08 -0300

    Tipos de dados e operadores

--- 1_introducao/ex001/index.js
+++ 1_introducao/ex001/index.js
@@ -1,4 +1,1 @@
-console.log("Hello World, funcionando o JavaScript.");// a função console.log serve para emitir uma porção de texto no console do navegador, ela é muito comum quando queremos ver um erro no nosso código ou algum valor de uma variável que é onde armazenamos o valor de dados. 
-
-
-
+console.log("Hello World, funcionando o JavaScript."); // a função console.log serve para emitir uma porção de texto no console do navegador, ela é muito comum quando queremos ver um erro no nosso código ou algum valor de uma variável que é onde armazenamos o valor de dados.
---
+++ 2_tipos_de_dados_e_operadores/1_number/index.js
@@ -0,0 +1,9 @@
+/*
+  NUMBERS 
+
+- Obviamente este tipo trata de números;
+- console.log(typeof 13);
+- console.log(typeof 1.8);
+- console.log(typeof -5); 
+ */
+
