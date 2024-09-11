15ba9a0500749709e8cb8b87432f0aadfa59f063
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-10 23:57:43 -0300

    number

--- 2_tipos_de_dados_e_operadores/1_number/index.js
+++ 2_tipos_de_dados_e_operadores/1_number/index.js
@@ -7,3 +7,14 @@
 - console.log(typeof -5); 
  */
 
+//Estes numeros serão exibidos no console
+console.log(5);
+console.log(12.7);
+console.log(-123);
+
+//como imprimir os tipos deles?
+//Para verificar o tipo dos números utilizados o método typeof que trará o tipo de dado do valor informado, no JS qualquer tipo de número é um number. 
+
+console.log(typeof 5);
+console.log(typeof 12.7);
+console.log(typeof -123);
