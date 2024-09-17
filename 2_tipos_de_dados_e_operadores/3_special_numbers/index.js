5e76d588a884b0bfea7033b3a5ae77f26fcad526
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-17 18:26:56 -0300

    special numbers

---
+++ 2_tipos_de_dados_e_operadores/3_special_numbers/index.js
@@ -0,0 +1,15 @@
+/*
+Special Numbers
+
+- Considerados números, mas não funcionam como números;
+- Infinity é quando temos uma operação aritmética errada que pode dar esse valor e é considerado como number.
+- -Infinity;
+- NaN (Not A Number) - o NaN acontece quando somamos alguma coisa que é numero com outra coisa que não é numero ou seja tratamos uma coisa que não é numero como número
+*/
+
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(typeof NaN);
+
+
