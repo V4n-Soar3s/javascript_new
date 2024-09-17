8f8e36ab4238a16c960bf346e3b3bbf7ee4574ce
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-17 20:30:59 -0300

    strings

---
+++ 2_tipos_de_dados_e_operadores/4_tipo_de_dado_string/index.js
@@ -0,0 +1,19 @@
+/*
+Strings
+
+- String === texto;
+- console.log(typeof 'isso é uma string');
+- console.log(typeof "este texto aqui também");
+- console.log(typeof `E este também`) - template literal;
+
+ */
+
+
+
+
+console.log(typeof "Oi, meu nome é Vanessa");//string
+console.log(typeof 'Eu queria comprar um carro');//string
+console.log(typeof `Teste`);//string
+console.log(typeof "Infinity");// string
+console.log(typeof Infinity);//special number 
+
---
+++ 2_tipos_de_dados_e_operadores/5_string_detalhes_mais_tecnicos/index.js
@@ -0,0 +1,19 @@
+/*
+Strings: Detalhes mais técnicos
+
+- A \ pode dar um 'escape' na string, e isso permite 'efeitos especiais';
+- Por exemplo: \n pula uma linha;
+- console.log("Essa é uma string \n de duas linhas");
+- Para inserir uma ' ou " devemos iniciar a String com a aspa inversa que desejamos inserir; 
+- O template literal serve para computar valores também, ex:
+- console.log(`A soma de 2 + 2 é ${2 + 2}`);
+- Concantenação é um processo de 'somar' Strings, veja:
+- console.log("salada" + "de" + "fruta") 
+*/
+ 
+
+//quebra de linha com \n
+
+console.log("Primeira linha \n Segunda linha");//pula a linha com \n
+
+
