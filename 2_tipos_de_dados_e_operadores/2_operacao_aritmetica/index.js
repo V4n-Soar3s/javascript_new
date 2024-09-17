7620754c2bd74701215ace8a30f5694f07806fdb
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-17 18:16:36 -0300

    operações aritméticas

---
+++ 2_tipos_de_dados_e_operadores/2_operacao_aritmetica/index.js
@@ -0,0 +1,49 @@
+//Number operações
+
+/*
+ Numbers: artimética
+
+- Operação mais feita com os números em JS;
+- E o resultado da operação aritmético produz um novo number;
+- console.log(2 + 2);
+- console.log(2 * 4 - 3);
+- console.log(8 / 4);
+- Funciona com a mesma da matemática;
+- console.log(5 + (2 * 4));
+
+Símbolos aritméticos:
+
++ === soma 
+- === subtração
+/ === divisão
+* === multiplicação
+% === resto
+
+ */
+
+console.log(typeof (2 + 3));//soma
+console.log(typeof (5 - 12));//subtração
+console.log(typeof (4 * 4));//multiplicação
+console.log(typeof (5 / 3));//divisão
+console.log(typeof (20 % 2));//resto da divisão
+console.log(typeof (21 % 2));//resto da divisao
+
+//todos retornam como numbers 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
