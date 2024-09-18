41fc65a66593846a15819623b8ca2bf2de653da1
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-17 21:34:00 -0300

    booleans comparacao

---
+++ 2_tipos_de_dados_e_operadores/7_booleans_comparacoes/index.js
@@ -0,0 +1,20 @@
+/*
+Booleans: comparações
+
+> maior que
+< menor que
+>= maior ou igual
+<= menor ou igual
+== igual 
+!= diferente
+=== idêntico 
+ */
+
+console.log(1 > 2);//false 
+console.log(5 < 10 );//true
+console.log(3 >= 3);//true
+console.log(5 <= 4);//false
+console.log( 5 == 4);//false
+console.log('Matheus' != 'Matheus');//false
+console.log(3 === "3");//false
+console.log(4 === 4);//true
