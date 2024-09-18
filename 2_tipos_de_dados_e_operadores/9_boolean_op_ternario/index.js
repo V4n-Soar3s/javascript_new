688cbf0a39f48cc064af2735aec2278539574448
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-17 22:20:13 -0300

    booleans_operadores_ternarios

---
+++ 2_tipos_de_dados_e_operadores/9_boolean_op_ternario/index.js
@@ -0,0 +1,18 @@
+//Operador Ternário - boolean
+
+/*
+
+Boolean : operador ternário
+
+ * Faz um comparativo em apenas uma linha de código;
+ * console.log(true?1:2) - lê se "É true? se sim é 1 senão é 2"
+ * console.log(false? 'falso':'verdadeiro')
+ * Não é muito utilizado e pode deixar o código complicado de ler
+ 
+ */
+
+console.log(5 > 2 ? 'Sim' : 'Não');//aqui estamos lendo 5 é maior que 2? Se for verdadeiro irá retornar a palavra sim, mas se a condição for falsa irá retornar não;
+console.log("Vanessa" == "Wanessa" ? 'Sim' : 'Não')
+console.log(true? 1:2)
+
+
