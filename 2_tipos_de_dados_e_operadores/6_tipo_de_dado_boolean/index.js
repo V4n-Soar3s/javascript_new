f33b50c452055c11f137fc6455c64e1caeb95f16
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-17 21:10:47 -0300

    booleans

---
+++ 2_tipos_de_dados_e_operadores/6_tipo_de_dado_boolean/index.js
@@ -0,0 +1,15 @@
+/**
+ * Booleans
+
+- Serve para guardar um valor de uma comparação, por exemplo;
+- Os unicos valores possiveis são:
+- True
+- False 
+- console.log(5>2);
+- console.log(3>10);
+ */
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof(10 > 2));
+console.log(typeof(5 > 200));
