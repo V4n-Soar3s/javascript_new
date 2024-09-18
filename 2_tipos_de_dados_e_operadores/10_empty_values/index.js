8cdf39398f5189f45d8673bd60399b3b71d1e720
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-17 22:26:45 -0300

    null && undefined

---
+++ 2_tipos_de_dados_e_operadores/10_empty_values/index.js
@@ -0,0 +1,12 @@
+/*
+ Empty values
+
+- Temos duas palavras reservadas da linguagem que servem para estes casos
+- undefined e null;
+- sempre que vc se deparar com estas palavras, o JS basicamente quer dizer que os valores não 
+existem;
+
+ */
+
+console.log(null);
+console.log(undefined);
