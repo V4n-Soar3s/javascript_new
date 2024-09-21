7b67db140a4ea15bfc0cb9eb600cf66ba2857c1e
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-21 10:56:35 -0300

    exercicios_primeira_sessao

---
+++ 2_tipos_de_dados_e_operadores/12_exercicios_dados_e_operadores/index.js
@@ -0,0 +1,25 @@
+//1 - Escreva três valores em string em um arquivo e exiba no navegador com o console.log(), um com aspas duplas, outro com simples e outro com template literals.
+
+console.log("Olá");//aspas duplas
+console.log('Vanessa');//aspas simples
+console.log(`Dia 20/09/2024`);//template literals
+
+//2 - Escreva três valores em number em um arquivo e exiba no navagador com o console.log(), com números inteiros, números com casa decimal e por aritmética:
+
+console.log(1);//numero inteiro
+console.log(14.8);//numero com casa decimal 
+console.log(12 * 65);//número por aritmética
+
+//3 - Escreva três comparações com boolean, uma com maior, menor ou igual e diferente
+
+console.log(5 > 2);//true
+console.log( 4 <= 4);//true
+console.log(!(4 != 3));//false
+
+//4 - Escreva três comparações com operadores lógicos, com and, or e not
+
+console.log( 3 <= 4 && 4 <= 4);// and
+console.log(4 <= 3 || 3 >= 10);//or
+console.log(!(4 >= 4 && 5 === 5))//not
+
+
