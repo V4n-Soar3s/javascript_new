b278edb9bbfe6c471b7d36235d6bd0af6152b12b
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-21 11:43:53 -0300

    declaração de variaveis

---
+++ 3_estruturas_de_programacao/1_variaveis/script.js
@@ -0,0 +1,24 @@
+let nome = "Vanessa"; // declarei uma variavel 
+
+console.log(nome);//chamei a variavel para ser apresentada no console 
+console.log(`Meu nome é ${nome}`)// imprimindo o valor da variavel no console utilizando template literals
+
+let laranjas = 5;// declarei uma variavel 
+console.log(laranjas * laranjas);//chamei a variavel para ser apresentada no console multiplicando seu valor 
+
+// como mudar o valor de uma variavel ja criada durante o programa 
+
+nome = "Bruna";// atribuimos um novo valor ao nome
+
+console.log(nome);// sera exibido o novo valor 
+
+laranjas = laranjas + 1;// sera atribuido +1 na variavel anterior que era igual a 5
+
+console.log(laranjas);// sera exibido o novo valor 6
+
+//podemos tbm no JS mudar o tipo da variavel, exemplo: 
+
+laranjas = "Laranja";//mudamos o tipo para string
+
+console.log(laranjas);//mudei o valor e o tipo para string 
+
