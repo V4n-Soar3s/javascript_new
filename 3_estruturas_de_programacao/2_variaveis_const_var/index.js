4cae4b60e3e48c022bef861d2d9b435c4f25f769
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-09-22 12:45:27 -0300

    var_const_let

---
+++ 3_estruturas_de_programacao/2_variaveis_const_var/index.js
@@ -0,0 +1,7 @@
+var nome = "Matheus";//pode ser alterado
+const ip = "127.0.0.1";// nao pode ser alterado
+
+console.log(nome);//imprimir o valor da variavel nome
+console.log(ip);// imprimir o valor da variavel ip
+
+ip = 12345;// quando fazemos isso sera apresentado erro no console do navegador, pois, estamos alterando o valor de uma constante e variaveis constantes nao podem ter novos valores atribuidos a elas 
