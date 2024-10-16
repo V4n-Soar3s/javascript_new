292b6d9f6580535499836d6e3fd1d948300a094c
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-10-15 21:08:32 -0300

    while

---
+++ 5_estruturas_de_repeticao/1_while/script.js
@@ -0,0 +1,33 @@
+/*
+
+Estrutura de repetição: while
+
+- Fará uma ação, até que a condição seja atingida;
+- Precisamos realmente 'definir um fim' para o loop´, para não termos o problema de loop infinito;
+
+*/
+
+let y = 0;
+
+while(y <= 20){
+    console.log(y);
+    y = y + 1;
+}
+
+// outro exemplo 
+
+let x = 10;
+
+while(x > 0){
+    console.log('O x é:' + x);
+    x = x - 1;
+}
+
+// outro exemplo
+
+let z = 30;
+
+while(z > 0){
+    console.log(z);
+    z = z - 1;
+}
