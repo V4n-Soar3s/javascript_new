fb8e881466c6228994470150818c4a9af55e5f20
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-10-25 22:24:31 -0300

    ex 001

---
+++ 11_exercicios_estrutura_de_programacao/001_exercicio/index.js
@@ -0,0 +1,14 @@
+/*
+
+- Armazene valores em variáveis com cada um dos tipos de dados vistos
+- String, number e boolean
+
+ */
+
+let nome = "Vanessa";
+let numero = 9;
+let cond_boolean = 5 > 2;
+
+console.log(typeof nome);
+console.log(typeof numero);
+console.log(typeof cond_boolean);
