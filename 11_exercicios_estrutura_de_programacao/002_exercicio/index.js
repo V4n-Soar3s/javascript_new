544f4f2623dfc601133297993a791612d2131817
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-10-25 22:40:39 -0300

    exercicio 03

---
+++ 11_exercicios_estrutura_de_programacao/002_exercicio/index.js
@@ -0,0 +1,7 @@
+//exercicio 02
+
+let idade = 19;
+
+if(idade >= 18){
+    console.log("Pode entrar");
+}
---
+++ 11_exercicios_estrutura_de_programacao/003_exercicio/index.js
@@ -0,0 +1,7 @@
+const nome = "Vanessa";
+
+if (nome == "Vanessa") {
+  console.log(`Olá ${nome}`);
+} else {
+  console.log("O nome não está na constante");
+}
