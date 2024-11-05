220212011ab837552b462a0e3ae7d79db1feebc5
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-11-05 20:54:58 -0300

    ex003

---
+++ 12_funcoes/013_ex_funcao003/script.js
@@ -0,0 +1,9 @@
+// Escreva uma função que some dois números e retorne eles; 
+//Depois imprima este retorno;
+
+function soma(x,y) {
+    return x + y;
+}
+
+console.log(soma(8, 10));
+console.log(soma(3, 2));
