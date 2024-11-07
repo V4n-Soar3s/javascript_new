080732bc04d748b2292c50479d396c79bdcc84f9
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-11-06 21:34:08 -0300

    ex007

---
+++ 12_funcoes/017_ex_funcao007/index.js
@@ -0,0 +1,13 @@
+/*
+Escreva uma função que receba um número negativo e retorne um número positivo;
+Dica: utilize a função Math.abs
+*/
+
+function retornarPositivo(a){
+    return Math.abs(a);
+}
+
+console.log(retornarPositivo(-4));
+console.log(retornarPositivo(-8));
+console.log(retornarPositivo(3));
+console.log(retornarPositivo(0));
