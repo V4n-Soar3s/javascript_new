cfc94a8cf1cf7ded1d26f7969a2ee4b083d60da3
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-11-13 19:31:46 -0300

    arrays

---
+++ 013_array/ex001_array/index.js
@@ -0,0 +1,19 @@
+//Ex 001 - Array 
+
+//Vamos declarar um array com uma sequência numérica 
+let arr = [1, 4, 5, 6, 7];//criei uma variavel e atribui o valor de array/conjunto numérico dentro dela 
+
+//Podemos cria arrays de nomes, exemplo: 
+let nomes = ['Matheus', 'João', 'Aléxia'];
+
+//Ou até mesmo arrays de booleanos:
+let bool = [true, false, true];// Não tem restrição quanto ao tipo de dado 
+
+//E também podemos criar arrays com tipos misturados, exemplo: 
+let misturado = [1, 'Matheus', true];
+
+//E também podemos exibir arrays no console: 
+console.log(arr);
+console.log(nomes);
+console.log(bool);
+console.log(misturado); 
