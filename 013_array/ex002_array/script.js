391a98335214392ebd549976bee58b337d0717e6
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-11-13 20:01:20 -0300

    como acessar o array

---
+++ 013_array/ex002_array/script.js
@@ -0,0 +1,14 @@
+//arrays 002 
+//Como acessar um elemento do array pelo seu indice: 
+
+let arr = [1, 4, 5, 6, 7];
+let nomes = ['Matheus', 'Vanessa', 'João', 'Aléxia'];
+let bool = [true, false, true];
+
+//acessar pelo indice
+console.log(arr[0]);//retorna 1
+console.log(arr[3]);//retorna 6
+console.log(nomes[0]);//retorna Matheus
+console.log(nomes[1]);//retorna Vanessa
+console.log(bool[0]);//retorna true
+console.log(bool[1]);//retorna false
