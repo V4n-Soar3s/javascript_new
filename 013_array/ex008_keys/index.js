2651571f53ae7edc099ccea5e8f53b4fd398caa7
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-11-24 13:37:32 -0300

    keys

---
+++ 013_array/ex008_keys/index.js
@@ -0,0 +1,12 @@
+// Keys = é um método que serve para verificar as chaves de um objeto exemplo: 
+
+let obj = {
+    'chave1': 1,
+    'chave2': 2,
+    'chave3': 3,
+}
+
+console.log(obj);//aqui irá fazer com que seja apresentado tudo o que há dentro do objeto
+
+console.log(Object.keys(obj));//chamei o objeto pai utilizando o método keys que irá apresentar apenas as chaves criadas dentro desse objeto no console
+
