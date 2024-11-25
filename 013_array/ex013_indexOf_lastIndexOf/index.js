482fca27cb2f0e0fddeefeba3b4cd707073206e8
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-11-25 20:32:06 -0300

    indexOf e lastIndexOf

---
+++ 013_array/ex013_indexOf_lastIndexOf/index.js
@@ -0,0 +1,7 @@
+//indexOf e lastIndexOf
+
+let nums = [5, 6, 2, 4, 9,6,2];//criei um array de números 
+
+console.log(nums.indexOf(6));//solicitei que seja informado onde aparece o primeiro indice que possui o elemento 06 do array, que retornara 1
+
+console.log(nums.lastIndexOf(6));//quando utilizo lastIndexOf eu faço com que o programa aponte qual foi a última posição do elemento 06 no array no caso retornará no informando que foi no indice 05
