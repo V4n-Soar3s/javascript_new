a7dd3509e6a5f23d931ef80419f3697e8e8a6824
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-11-24 22:55:49 -0300

    push e pop

---
+++ 013_array/ex011_push_pop/script.js
@@ -0,0 +1,11 @@
+//push e pop
+
+let nomes = ['Vanessa', 'Bruna', 'Julia', 'Larissa'];//criei o array
+
+let elementoRemovido = nomes.pop();//solicitei que seja removido o utilmo elemento e dei um nome a ele
+
+console.log(elementoRemovido);//solicitei que seja exibido o elemento removido
+console.log(nomes);//solicitei que seja apresentado o array após a remoção do ultimo elemento 
+
+nomes.push('Mariana');//aqui estou inserindo um elemento 
+console.log(nomes);//quando for exibido será com o elemento novo Mariana no final 
