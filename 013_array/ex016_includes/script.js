a2cecd5bb4f26bef21945b3100e5ad859381f650
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-11-27 18:58:46 -0300

    includes

---
+++ 013_array/ex016_includes/script.js
@@ -0,0 +1,12 @@
+//método includes = serve para verificar se um determinado elemento existe dentro do array, se o mesmo existir é retornada a mensagem true se não false 
+
+//criei o array
+let racas = ["Poodle", "Beagle", "Buldogue", "Boxer", "Maltês"];
+
+//vamos utilizar o método no console
+console.log(racas.includes("Pug"));//retorna false pois nao existe no array
+console.log(racas.includes("Beagle"));//retorna true pois existe no array 
+
+if(racas.includes("Beagle")){
+    console.log('Este cachorro pertence a Vanessa')
+}
