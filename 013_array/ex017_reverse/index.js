3143bd6b19583a497e14f1480ec897f4b3518340
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-12-01 16:32:20 -0300

    reverse

---
+++ 013_array/ex017_reverse/index.js
@@ -0,0 +1,8 @@
+//método reverse 
+
+//Inverte um array
+
+let arr = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(arr.reverse());//o array sera apresentado de trás pra frente, seja com números ou qualquer outro tipo de dado ou valor que ele receba desde que seja um array 
+
