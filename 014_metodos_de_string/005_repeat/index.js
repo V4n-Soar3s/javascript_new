b3e919e71836932dea7b5bb6b3361761bd249183
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2024-12-17 19:44:55 -0300

    repeat

--- 014_metodos_de_string/005_repeat/index.js
+++ 014_metodos_de_string/005_repeat/index.js
@@ -1,6 +1,10 @@
 //Método repeat
 
 //criar uma variavel para armazenar uma palavra
-let palavra = 'Oi';
+let palavra = 'Oi ';
 
-console.log(palavra.repeat(8));
+console.log(palavra.repeat(8));//solicitei chamando o método que a palavra oi seja repetida 8x
+
+let frase = "Repetir a frase ";
+
+console.log(frase.repeat(10));
