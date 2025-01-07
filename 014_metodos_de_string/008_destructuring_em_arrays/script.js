f9bc254297be3e31e5d8a4f39eb7c6da06cfe5da
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-01-06 21:22:06 -0300

    destructuring arrays

---
+++ 014_metodos_de_string/008_destructuring_em_arrays/script.js
@@ -0,0 +1,12 @@
+//Destructuring com arrays 
+
+//Podemos definir variáveis com os valores de um array utilizando o destructuring 
+
+//vamos criar um array 
+let nomes = ['Matheus', 'João', 'Pedro'];
+
+let [nomeA, nomeB, nomeC] = nomes;//aqui criamos uma variavels e desestruturamos o array, fazendo com que cada propriedade dele tenha uma variavel ao invocarmos, veja abaixo: 
+
+console.log(nomeA);
+console.log(nomeB);
+console.log(nomeC);
