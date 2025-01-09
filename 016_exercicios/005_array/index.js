6fba6f1a5d8799e902b9041da593d1c962189818
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-01-09 20:21:04 -0300

    include

--- 016_exercicios/005_array/index.js
+++ 016_exercicios/005_array/index.js
@@ -1,11 +1,14 @@
 /*Crie um array com 5 nomes, incluindo o seu, verifique se o seu nome existe no array, se existir imprima alguma mensagem no console */
 
+//criei o array
 let nomes = ["Bruna", "Vanessa", "Guilherme", "Carlos", "Adriana"];
 
+//solicitei que seja verificado se existe o nome vanessa dentro dele 
 if (nomes.includes("Vanessa")) {
   console.log(`O nome ${nomes[1]} consta na lista`);
 }
 
+//solicitei que seja verificado se existe o nome ana dentro dele 
 if (nomes.includes("Ana")) {
   console.log(`Este nome existe na lista`);
 } else {
