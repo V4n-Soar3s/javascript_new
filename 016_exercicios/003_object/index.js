4e3f1cb73054e37eae2a0f322c04bc4e8ddaee1b
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-01-09 19:45:48 -0300

    ex003_object

---
+++ 016_exercicios/003_object/index.js
@@ -0,0 +1,14 @@
+/*
+- Crie um objeto onibus, nele deve conter as propriedades rodas = 8, limite de passageiros = 40 e portas = 2, após isso realize a impressão das propriedades no console;
+ */
+
+let onibus = {
+    rodas:8,
+    limitePassageiros: 40,
+    portas: 2
+}
+
+console.log(onibus);
+console.log(onibus.rodas);
+console.log(onibus.limitePassageiros);
+console.log(onibus.portas);
