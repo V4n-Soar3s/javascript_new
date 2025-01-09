09aa958c7390cff2097e7735ec90e272d5c43a6a
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-01-09 20:39:40 -0300

    verificando quantidades

---
+++ 016_exercicios/006_array/script.js
@@ -0,0 +1,14 @@
+let frutas = ["maçã", "banana", "uva", "caqui", "morango", "abacaxi"];
+let legumes = ["cenoura", "beterraba", "batata"];
+
+function verificaQuantidadeElementos(arr) {
+    if (arr.length >= 5) {
+        console.log("Muitos elementos");
+    } else {
+        console.log("Poucos elementos");
+    }
+}
+
+// Chamando a função para verificar cada array
+verificaQuantidadeElementos(frutas);  // Deve imprimir "Muitos elementos"
+verificaQuantidadeElementos(legumes); // Deve imprimir "Poucos elementos"
