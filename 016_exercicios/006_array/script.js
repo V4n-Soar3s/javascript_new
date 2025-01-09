36d0319df5f1788df942cc97104e9dd6f63597c2
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-01-09 20:56:06 -0300

    arrays

--- 016_exercicios/006_array/script.js
+++ 016_exercicios/006_array/script.js
@@ -2,13 +2,13 @@ let frutas = ["maçã", "banana", "uva", "caqui", "morango", "abacaxi"];
 let legumes = ["cenoura", "beterraba", "batata"];
 
 function verificaQuantidadeElementos(arr) {
-    if (arr.length >= 5) {
-        console.log("Muitos elementos");
-    } else {
-        console.log("Poucos elementos");
-    }
+  if (arr.length >= 5) {
+    console.log("Muitos elementos");
+  } else {
+    console.log("Poucos elementos");
+  }
 }
 
 // Chamando a função para verificar cada array
-verificaQuantidadeElementos(frutas);  // Deve imprimir "Muitos elementos"
+verificaQuantidadeElementos(frutas); // Deve imprimir "Muitos elementos"
 verificaQuantidadeElementos(legumes); // Deve imprimir "Poucos elementos"
---
+++ 016_exercicios/007_array/index.js
@@ -0,0 +1,8 @@
+//ex 007
+
+let nomes = ["Nilda", "Juvenal", "Sinemar", "Leandra", "Simone"];
+
+//loopping com for 
+for( let i = 0; i < nomes.length; i++){
+    console.log(nomes[i]);
+}
