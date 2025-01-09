302af749dec3c0ae4ddb3b7baad1b28fbaf51020
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-01-09 18:57:12 -0300

    exercicio de array

---
+++ 016_exercicios/001_array/index.js
@@ -0,0 +1,16 @@
+/*
+Exercicio 01 
+
+- Crie um array com 5 itens;
+- Acesse o item 1, 3, 4;
+- Obs: arrays começam no índice 0;
+
+*/
+
+//criei o array
+let numeros = [1, 2, 3, 4 ,5];
+
+//solicitei que apenas os indices 1, 3 e 4 sejam apresentados no console 
+console.log(numeros[1]);
+console.log(numeros[3]);
+console.log(numeros[4]);
