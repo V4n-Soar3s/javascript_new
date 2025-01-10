24896bf2626722728fc95dde8d0e5c73c12e6e45
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-01-09 21:02:03 -0300

    json

---
+++ 016_exercicios/008_json/index.js
@@ -0,0 +1,12 @@
+//Crie um json com 3 propriedades, atribua ele a uma variavel, acesse as propriedades imprimindo no console
+
+
+let person = {
+    "name":"João",
+    "age": 32, 
+    "city": "São Paulo",
+}
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
