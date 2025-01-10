cf5f892aa890411b97c956b41c901685539f3c78
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-01-09 21:14:02 -0300

    split

---
+++ 016_exercicios/009_array/script.js
@@ -0,0 +1,10 @@
+// Define a frase
+let frase = "O rato roeu a roupa do rei de Roma";
+
+// Converte a frase em um array de palavras
+let palavras = frase.split(" ");
+
+// Imprime cada palavra no console
+for (let i = 0; i < palavras.length; i++){
+console.log(palavras[i]);
+}
