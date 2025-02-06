13e85bceecfa7fdb7b5c727ca472a1dbed9a57b0
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-02-06 08:40:55 -0300

    try and catch

---
+++ 019_bugs_debugs/006_try_catch/script.js
@@ -0,0 +1,13 @@
+//try e catch
+
+//let b = 2;
+
+try {
+  let a = 2 + b;//caso retorne algum erro aqui dentro 
+} catch (error) {//é capturado pelo catch
+  console.log(error);//e apresentado no console, no caso será apresentado que a variavel b não foi definida
+}
+
+
+//O try catch diferente do error ele não para o código diferente do error, podemos ver isso utilizando uma leitura no console: 
+console.log('teste');
