c130b61dcdc86e9437368836978005283e9ef0ee
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-02-12 17:04:29 -0300

    elementos tag

---
+++ 023_dom/003_elementos_tag/js/script.js
@@ -0,0 +1,9 @@
+//método get.ElementByTagName("h1"); - conseguimos encontrar elemento no dom pela sua tag
+
+console.log(document.getElementsByTagName('h1'));
+
+//também podemos fazer isso com parágrafos
+console.log(document.getElementsByTagName('p'));
+
+//listas
+console.log(document.getElementsByTagName('li'));
