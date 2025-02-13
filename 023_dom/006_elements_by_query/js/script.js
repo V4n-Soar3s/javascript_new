2e91b1b97b34de15cbb5e5899ea6c746e6b7f2a9
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-02-13 11:21:49 -0300

    by query

---
+++ 023_dom/006_elements_by_query/js/script.js
@@ -0,0 +1,11 @@
+//como selecionar com query 
+console.log(document.querySelector('#container-principal h1'));
+//Obs: quando utilizamos # estamos informando que se trata do id e depois podemos colocar um espaço informando a tag que é filha dele 
+
+//também podemos acessar apenas um elemento com query selector
+console.log(document.querySelector('h1'));
+
+//pra acessar uma classe
+console.log(document.querySelector('ul .itens-azuis'));
+// o ponto representa classes
+
