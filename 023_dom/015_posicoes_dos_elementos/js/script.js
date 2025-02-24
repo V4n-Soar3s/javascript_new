03ae0341e927a8f1c9844ead12e0e963525b7559
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-02-24 10:16:58 -0300

    position_of_element

---
+++ 023_dom/015_posicoes_dos_elementos/js/script.js
@@ -0,0 +1,6 @@
+//Como verificar a posição de algum elemento na tela?
+
+let elemento = document.querySelector("#titulo-principal");
+
+//se eu quiser saber a posição do elemento h1 usarei o método abaixo: 
+console.log(elemento.getBoundingClientRect());
