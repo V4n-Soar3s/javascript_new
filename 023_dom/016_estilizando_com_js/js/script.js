a1d38830f081a1b7927b4d94bfcd460190bd6c9a
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-02-24 10:38:03 -0300

    style_css_and_js

---
+++ 023_dom/016_estilizando_com_js/js/script.js
@@ -0,0 +1,9 @@
+//como alterar CSS utilizando o JS?
+
+//vamos criar uma variavel pra chamar o elemento 
+let elemento = document.querySelector('#titulo-principal');
+
+//iremos acessar a propriedade style desse elemento e alterar sua cor
+elemento.style.color = 'red';
+//alterar a largura 
+elemento.style.width = '400px';
