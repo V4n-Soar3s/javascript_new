d353c2006b595dc761f1a2cf133e101fcddb11ed
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-02-25 14:15:00 -0300

    evento scroll

---
+++ 024_eventos/008_eventos_scroll/js/script.js
@@ -0,0 +1,9 @@
+//evento com scroll
+
+window.addEventListener("scroll", function(e){
+
+    if(window.scrollY > 1000){
+        console.log("Ativou");
+    }
+
+});//quando nosso eixo Y chegar em 1000 será apresentada a mensagem ativou
