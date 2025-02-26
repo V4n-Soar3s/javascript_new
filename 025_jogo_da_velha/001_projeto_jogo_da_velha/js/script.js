002cc3b9c0f77cd530566ca884a827e33ab6d244
V4n-Soar3s <163613892+V4n-Soar3s@users.noreply.github.com>
2025-02-26 15:20:39 -0300

    update

--- 025_jogo_da_velha/001_projeto_jogo_da_velha/js/script.js
+++ 025_jogo_da_velha/001_projeto_jogo_da_velha/js/script.js
@@ -1,1 +1,41 @@
-console.log("teste");
+let x = document.querySelector(".x");
+let o = document.querySelector(".o");
+let boxes = document.querySelectorAll(".box");
+let buttons = document.querySelectorAll("#buttons-container button");
+let messageContainer = document.querySelector("#message");
+let messageText = document.querySelector("#message p");
+let secondPlayer;
+
+//contador de jogadas
+let player1 = 0;
+let player2 = 0;
+
+//adicionando o evento de click aos boxes
+for (let i = 0; i < boxes.length; i++) {
+  // quando alguem clica na caixa
+  boxes[i].addEventListener("click", function () {
+    let el;
+
+    if (player1 == player2) {
+      // x
+      el = x;
+    } else {
+      // o
+      el = o;
+    }
+
+    //verifca se já tem x ou bola
+    if (this.childNodes.length == 1) {
+      let cloneEl = el.cloneNode(true);
+
+      this.appendChild(cloneEl);
+
+      //computar jogada
+      if (player1 == player2) {
+        player1++;
+      } else {
+        player2++;
+      }
+    }
+  });
+}
